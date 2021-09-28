f1a1e26514e4888961c162bf831197427bdf0e18
cnguu <www@cnguu.cn>
2021-09-28 17:55:24 +0800

    build(tokens): fix token build script

--- packages/tokens/gulpfile.js
+++ packages/tokens/gulpfile.js
@@ -1,43 +1,36 @@
 import path from 'path'
-import gulp from 'gulp'
-import { buildOutput } from '../../build/utils/paths'
+import { src, dest, parallel, series } from 'gulp'
+
+import { epOutput } from '../../build/utils/paths'
 import rewriter from '../../build/gulp-rewriter'
+import { buildConfig } from '../../build/info'
+import { withTaskName } from '../../build/utils/gulp'
 
-export const esm = './es'
-export const cjs = './lib'
-
-const inputs = [
-  './*.js',
-  '!./node_modules',
-  '!./gulpfile.js',
-  '!./__tests__/*.js',
-]
-
-function compileEsm() {
-  return gulp.src(inputs).pipe(rewriter('..')).pipe(gulp.dest(esm))
+export const buildTokens = (module) => {
+  const inputs = [
+    './*.js',
+    '!./node_modules',
+    '!./gulpfile.js',
+    '!./__tests__/*.js',
+  ]
+  const config = buildConfig[module]
+  return withTaskName(`buildTokens:${module}`, () =>
+    src(inputs)
+      .pipe(rewriter('..'))
+      .pipe(dest(path.resolve(__dirname, config.output.name)))
+  )
 }
 
-function compileCjs() {
-  return gulp.src(inputs).pipe(rewriter('..')).pipe(gulp.dest(cjs))
+const copyTokens = (module) => {
+  const config = buildConfig[module]
+  return withTaskName(`copyTokens:${module}`, () => {
+    return src(`${path.resolve(__dirname, config.output.name)}/**`).pipe(
+      dest(path.resolve(epOutput, config.output.name, 'tokens'))
+    )
+  })
 }
 
-const distBundle = path.resolve(buildOutput, './spider-nest-vue')
-
-/**
- * copy from packages/hooks/lib to dist/hooks
- */
-function copyEsm() {
-  return gulp
-    .src(`${esm}/**`)
-    .pipe(gulp.dest(path.resolve(distBundle, './es/tokens')))
-}
-
-function copyCjs() {
-  return gulp
-    .src(`${cjs}/**`)
-    .pipe(gulp.dest(path.resolve(distBundle, './lib/tokens')))
-}
-
-export const build = gulp.series(compileEsm, compileCjs, copyEsm, copyCjs)
-
-export default build
+export default parallel(
+  series(buildTokens('cjs'), copyTokens('cjs')),
+  series(buildTokens('esm'), copyTokens('esm'))
+)
